7b471595cd5f471e585ef0cdcf079d43f12d8fe7
hoinlee-moi <lhi3048@gmail.com>
2022-05-20 20:00:39 +0900

    0520java 타임어택 구현 테스트 (하나도 못함)

--- 0506/app.py
+++ 0506/app.py
@@ -15,13 +15,22 @@ def index():
 
 @app.route('/detail/<idx>')
 def detail(idx):
-    post = db.test.find_one({'idx': int(idx)}, {'_id': False})
-    return render_template("detail.html", post=post)
+    article = db.test.find_one({'idx': int(idx)}, {'_id': False})
+
+    return render_template("detail.html", article=article)
     # todo
 
 @app.route('/articleList', methods=['GET'])
 def get_article_list():
-    article_list = list(db.homework.find({}, {'_id': False}).sort('_id', -1))
+    order = request.args.get('order')
+    if order == "desc":
+        article_list = list(db.homework.find({}, {'_id': False}).sort([("read_count", -1)]))
+    else:
+        article_list = list(db.homework.find({}, {'_id': False}).sort([("read_count", 1)]))
+
+    for article in article_list:
+        article['date'] = article['date'].strftime('%Y.%m.%d %H:%M:%S')
+
     return jsonify({"article_list": article_list})
 
 # Create
@@ -33,11 +42,11 @@ def create_article():
     else:
         idx = list(db.homework.find({}, sort=[('_id', -1)]).limit(1))[0]['idx'] + 1
 
-    today = datetime.now()
+
     title_give = request.form['title']
     content_give = request.form['content']
     pw_give = request.form['pw']
-    reg_date = today.strftime('%Y년%m월%d일 %H시%M분%S초')
+
 
 
     doc={
@@ -45,7 +54,7 @@ def create_article():
         'title':title_give,
         'content':content_give,
         'pw': pw_give,
-        'date':reg_date,
+        'date':datetime.now(),
         'read_count': 0
     }
 
@@ -55,18 +64,26 @@ def create_article():
 # Read
 @app.route('/article', methods=['GET'])
 def read_article():
-    article = 0 #todo
+    idx = request.args['idx']
+    db.article.update_one({'idx':int(idx)}, {'$inc': {'read_count':1}})
     return jsonify({"article": article})
 
 # Update
 @app.route('/article', methods=['PUT'])
 def update_article():
+    idx = request.form.get('idx')
+    title = request.form.get('title')
+    content = request.form.get('content')
+
+    db.homework.update_one({'idx': int(idx)}, {'$set': {'title':title, 'content':content}})
     # todo
     return {"result": "success"}
 
 # Delete
 @app.route('/article', methods=['DELETE'])
 def delete_article():
+    idx = request.args.get('idx')
+    db.article.delete_one({'idx':int(idx)})
     # todo
     return {"result": "success"}
 
